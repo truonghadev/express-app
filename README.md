cdc5f0d310c64a04cc0e52ef110c63aaced6a4df
truong.ha <truong.ha.dev@gmail.com>
2017-07-05 21:38:19 +0700

    update structure

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Run:
   set DEBUG=myapp:* & npm start
+
+Server: https://enigmatic-thicket-64480.herokuapp.com/
--- app_server/index/index.js
+++ app_server/index/index.js
@@ -1,9 +1,8 @@
+var controller = require('./index.controller');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', controller.index);
 
 module.exports = router;
